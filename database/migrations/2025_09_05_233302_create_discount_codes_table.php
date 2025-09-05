8c7e4d7a3fb40695eca57c3304f3b5e88c057e37
MahmoudEl-deeb <Mahmoud_ahmed_eldeeb@outlook.com>
2025-09-06 02:39:19 +0300

    feat: Add DiscountCode model and migration
    This commit introduces the foundational components for a discount code system.
    
    It includes a new `DiscountCode` Eloquent model and a corresponding database migration to create the `discount_codes` table. This provides the necessary database structure and application-level interface for managing discount codes.

---
+++ database/migrations/2025_09_05_233302_create_discount_codes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discount_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->enum('type', ['fixed', 'percentage']);
+            $table->decimal('value', 10, 2);
+            $table->decimal('max_discount_amount', 10, 2);
+            $table->datetime('valid_from');
+            $table->datetime('valid_to');
+            $table->integer('usage_limit');
+            $table->integer('times_used');
+            $table->boolean('is_active')->default('true');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discount_codes');
+    }
+};
