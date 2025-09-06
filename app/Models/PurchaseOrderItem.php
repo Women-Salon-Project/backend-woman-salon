859c6a483fe150e15e59c9ae2bae7b2494e56315
MohamedRagab <alys281.fare@gmail.com>
2025-09-06 13:52:51 +0300

    Add PurchaseOrderItem model and migration

---
+++ app/Models/PurchaseOrderItem.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PurchaseOrderItem extends Model
+{
+     protected $fillable = [
+        'purchase_order_id',
+        'product_id',
+        'quantity_ordered',
+        'quantity_received',
+        'unit_price',
+        'total_price',
+        'notes',
+    ];
+    public function purchaseOrder()
+    {
+        return $this->belongsTo(PurchaseOrder::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
